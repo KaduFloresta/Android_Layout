a1c68b5ad196fc47eddcbe85be3a7bf11d8c440b
Kadu Floresta <cefloresta1@gmail.com>
2021-04-02 19:30:07 -0300

    Initial commit

---
+++ app/src/main/java/com/example1/android_layout/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example1.android_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+    // Mudanças no layout usando o themes, colors e strings.xml
+
+    // Criação de Strings usando o strings.xml
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
